28266ddd761c5cad74d2806d001b9b1bf8ad0ead
alreadynyeng <kjw87130@naver.com>
2022-11-23 16:07:42 +0900

    #6 feat : 버튼 컴포넌트 생성

---
+++ src/component/ButtonComponent.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+
+export function GreenButton({text}) {
+  return (
+    <TouchableOpacity
+    style={styles.GreenButton}>
+    <Text style={styles.white}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export function WhiteButton({text}) {
+    return (
+      <TouchableOpacity
+      style={styles.WhiteButton}>
+      <Text style={styles.red}>{text}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+
+
+const styles = StyleSheet.create({
+    GreenButton : {
+        backgroundColor : "#6A7759",
+        borderRadius: 20,
+        width : 110,
+        height : 40,
+        justifyContent: 'center',
+    },
+    white : {
+        textAlign : 'center',
+        color : 'white',
+        fontSize : 20,
+        fontWeight : '300'
+    },
+    WhiteButton : {
+        backgroundColor : "white",
+        borderRadius: 10,
+        width : 75,
+        height : 40,
+        justifyContent: 'center',
+    },
+    red : {
+        textAlign : 'center',
+        color : '#F90C0C',
+        fontSize : 20,
+        fontWeight : '300'
+    }
+
+});
