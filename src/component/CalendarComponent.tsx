4fde26a552b6c7f6cf77c0cf443dedbfb0ad6fba
alreadynyeng <kjw87130@naver.com>
2022-12-01 14:33:13 +0900

    #9 feat : 달력화면 구현

---
+++ src/component/CalendarComponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Calendar } from 'react-native-calendars';
+import { StyleSheet } from 'react-native'
+
+export default function CalendarComponent() {
+
+  return (
+  <Calendar 
+  style={styles.calendar} 
+  theme= {{
+    selectedDayBackgroundColor: '#6A7759',
+    arrowColor : '#6A7759',
+    // todayTextColor : 'blue',
+    // dotColor : 'green'
+  }}
+  />
+  )
+}
+
+const styles = StyleSheet.create({
+    Calendar : {
+        borderBottomWidth : 1,
+        borderBottomColor : '#e0e0e0'
+    }
+})
--- src/screens/Nav/CalendarScreen.tsx
+++ src/screens/Nav/CalendarScreen.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+import CalendarComponent from '../../component/CalendarComponent';
 
 export default function Calender() {
   return (
-    <View>
-      <Text>Calender</Text>
+    <View style={styles.container}>
+      <CalendarComponent />
+      
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    backgroundColor : "white"
+  }
+
+});
