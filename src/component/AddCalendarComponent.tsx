33c83d06e86555ea0c12ea4ba7ab4cdafae9e222
alreadynyeng <kjw87130@naver.com>
2022-12-05 02:22:33 +0900

    #9 feat : 임시 드롭다운 구현

--- src/component/AddCalendarComponent.tsx
+++ src/component/AddCalendarComponent.tsx
@@ -1,54 +1,64 @@
-import { View, Text, StyleSheet, TextInput, Keyboard } from 'react-native'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  TextInput 
+} from 'react-native'
+import DropDownPicker from 'react-native-dropdown-picker';
 
-import { GreenButton } from './ButtonComponent'
-
-export default function AddCalendarComponent({navigation}) {
-
-  const [text, setText] = useState('');
-
-  const Input = ()=>{
-    setText('');
-    Keyboard.dismiss();
-    console.log(text)
-}
-
-const Cancle = ()=>{
-    if (navigation?.canGoBack()){
-        navigation.goBack()
-        return true
-    }
-    return false
-}
-
+export default function AddCalendarComponent() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+      {label: 'O', value: '1'},
+      {label: 'X', value: '2'},
+  ]);
   return (
     <View style={styles.container}>
       <View style={styles.line}>
         <Text style={styles.txt}>TITLE</Text>
         <TextInput
-        style = {styles.title}
+        style = {styles.in}
         ></TextInput>
       </View>
 
       <View style={styles.line}>
         <Text style={styles.txt}>DATE</Text>
         <TextInput
-        style = {styles.title}
+        style = {styles.in}
         ></TextInput>
       </View>
 
       <View style={styles.line}>
         <Text style={styles.txt}>TIME</Text>
         <TextInput
-        style = {styles.title}
+        style = {styles.in}
         ></TextInput>
       </View>
 
       <View style={styles.line}>
         <Text style={styles.txt}>ALERT</Text>
-        <TextInput
-        style = {styles.title}
-        ></TextInput>
+        <View>
+        <DropDownPicker
+          style= {styles.alert}
+          textStyle={{
+            fontSize: 15,
+            color : '#6A7759'
+          }}
+          labelStyle={{
+            color : '#6A7759'
+          }}   
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          placeholder="Alert"
+        />
+        </View>
+ 
       </View>
 
     </View>
@@ -63,13 +73,13 @@ const styles = StyleSheet.create({
         padding : 40,
         borderRadius : 10,
     },
-    title : {
+    in : {
         width : 160,
         height : 30,
         borderRadius : 10,
         backgroundColor : '#FCF4D6',
         padding : 10,
-        marginLeft : 20
+        marginLeft : 40
     },
     line : {
         flexDirection : 'row',
@@ -81,4 +91,9 @@ const styles = StyleSheet.create({
         fontSize : 20,
         fontWeight : '300'
     },
+    alert : {
+      width : 90,
+      marginRight : 70,
+      backgroundColor : '#FCF4D6',
+    }
 })
