a5be5d7e1d2b14f0b2086c000723d58619b7ec16
정민 <90408458+jeonjmin@users.noreply.github.com>
2022-12-18 15:51:31 +0900

    #7 feat : DateTimePickerModal 생성

--- src/screens/GalleryWriteScreen.tsx
+++ src/screens/GalleryWriteScreen.tsx
@@ -1,4 +1,7 @@
 import React, {useState} from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 
 import { 
   View, 
@@ -10,16 +13,26 @@ import {
   TouchableOpacity
 } from 'react-native'
 
-
-
+import { GreenButton } from '../component/ButtonComponent'
+import Home from '../component/DateSelectComponent';
 export default function GalleryWriteScreen({navigation}) {
+
+  const toGoGalleryPage = () =>{
+    navigation.navigate('Gallery')
+  }
+
+
+
   return (
   <SafeAreaView style={styles.container}>
-    <View style={styles.selectDate}></View>
+    <View style={styles.selectDate}>
+     <Home></Home>
+    </View>
     <View style={styles.selectPhoto}></View>
     <View style={styles.writeMemo}></View>
-    <View style={styles.continueBtn}></View>
-
+    <View style={styles.continueBtn}>
+    <GreenButton text='continue' on={toGoGalleryPage}/>
+    </View>
 
   </SafeAreaView>
   );
@@ -28,6 +41,24 @@ const styles = StyleSheet.create({
   container : {
     flex : 1,
     backgroundColor : "white",
-    padding : 100,
    },
+
+   selectDate:{
+    flex : 1,
+    padding:20,
+    },
+   selectPhoto:{
+    flex : 4,
+    backgroundColor: 'yellow',
+
+   },
+   writeMemo:{
+    flex : 5,
+    backgroundColor: 'purple',
+
+   },
+   continueBtn:{
+    flex : 1,
+    left:150
+   }
 });
