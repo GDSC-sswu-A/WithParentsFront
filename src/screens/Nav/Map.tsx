ddd4b5d5858d4b3737ac12b0d78375a3ed6f1186
Leeminhyeong <Leeminhyeong@MacBook-Pro.local>
2023-01-15 15:11:26 +0900

    #10 [feat]구글 맵 api를 활용해서 지도 구현

--- src/screens/Nav/Map.tsx
+++ src/screens/Nav/Map.tsx
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
-import MapView from 'react-native-maps'
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'
+import MapViewComponent from '../../component/MapViewComponent'
 
 export default function Map() {
   return (
-    <View>
-      <Text>Map</Text>
-      <MapView/>
+    <View style={styles.container}>
+      <View style={styles.map}>
+      <MapView
+          style={styles.container}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container : {
+   flex : 1,
+   backgroundColor : "white",
+  },
+  map : {
+    // flex : 1,
+    margin : 10,
+    height : 500
+
+    
+  }
+  
+});
