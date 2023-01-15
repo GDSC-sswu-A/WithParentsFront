d0ce2beeae3467dfc7d9f8094c40ba0d1ad8f353
Leeminhyeong <Leeminhyeong@MacBook-Pro.local>
2023-01-15 16:13:23 +0900

    #10 [feat] 지도화면 구현

--- src/component/MapViewComponent.tsx
+++ src/component/MapViewComponent.tsx
@@ -1,22 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import MapView, {PROVIDER_GOOGLE} from "react-native-maps"
 
 export default function MapViewComponent() {
     return (
         <>
-          <View style={{ flex: 1 }}>
-            <MapView
-              style={{ flex: 1 }}
-              provider={PROVIDER_GOOGLE}
-              initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-              }}
-            />
+          <View style={styles.container}>
+          <MapView
+          style={styles.container}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
           </View>
         </>
       );
 }
+const styles = StyleSheet.create({
+  container : {
+   flex : 1,
+   backgroundColor : "white",
+   height : 200,
+   width :280,
+   padding : 100 
+},
+  
+});
--- src/screens/Nav/Map.tsx
+++ src/screens/Nav/Map.tsx
@@ -1,11 +1,22 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'
 import MapViewComponent from '../../component/MapViewComponent'
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function Map() {
+  const [open, setOpen] = useState(false);
+  const [parent, setParent] = useState(null);
+  const [parents, setParents] = useState([
+      {label: '아부지', value: '1'},
+      {label: '어무니', value: '2'},
+  ]);
+  console.log(parents[0].label)
+
   return (
     <View style={styles.container}>
+      
+      <Text style={styles.title}>Parent's location</Text>
       <View style={styles.map}>
       <MapView
           style={styles.container}
@@ -18,6 +29,30 @@ export default function Map() {
           }}
         />
       </View>
+      <Text style={styles.title}>The last connection time</Text>
+      <Text style={styles.date}>2022-10-11 18:32</Text>
+      <View>
+      <DropDownPicker
+        // style = {{backgroundColor : '#FCF4D6'}}
+        containerStyle={{
+          width : 100
+        }}
+        textStyle={{
+          fontSize: 13,
+          color : '#6A7759'
+        }}
+        labelStyle={{
+          color : '#6A7759'
+        }}   
+        open={open}
+        value={parent}
+        items={parents}
+        setOpen={setOpen}
+        setValue={setParent}
+        setItems={setParents}
+        placeholder="parents"
+      />
+      </View>
     </View>
   )
 }
@@ -25,13 +60,22 @@ const styles = StyleSheet.create({
   container : {
    flex : 1,
    backgroundColor : "white",
+   padding : 20
   },
   map : {
     // flex : 1,
-    margin : 10,
-    height : 500
-
-    
+    // margin : 10,
+    height : 320,
+    marginBottom : 40
+  },
+  title : {
+    backgroundColor : "#EFF3EA",
+    fontSize : 20,
+    fontWeight: '300',
+    padding : 5,
+    marginBottom : 20
+  },
+  date : {
+    marginBottom : 40
   }
-  
 });
