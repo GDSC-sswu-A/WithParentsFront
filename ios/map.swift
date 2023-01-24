0389d180af6cc1345a91af4b95606bec6c42a4f5
Leeminhyeong <Leeminhyeong@MacBook-Pro.local>
2023-01-24 20:27:59 +0900

    #18 [feat] 사용자의 현재위치 불러오기 및 마커 생성

---
+++ ios/map.swift
@@ -0,0 +1,8 @@
+//
+//  map.swift
+//  withParents
+//
+//  Created by Leeminhyeong on 2023/01/24.
+//
+
+import Foundation
--- src/screens/Nav/Map.tsx
+++ src/screens/Nav/Map.tsx
@@ -1,18 +1,44 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React, {useState} from 'react'
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'
+import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'
 import MapViewComponent from '../../component/MapViewComponent'
+import Geolocation from 'react-native-geolocation-service'
 import DropDownPicker from 'react-native-dropdown-picker';
 
+interface ILocation {
+  latitude : number;
+  longitude : number;
+}
 export default function Map() {
+  const [location, setLocation] = useState<ILocation | undefined>(undefined);
   const [open, setOpen] = useState(false);
   const [parent, setParent] = useState(null);
   const [parents, setParents] = useState([
       {label: '아부지', value: '1'},
       {label: '어무니', value: '2'},
   ]);
-  console.log(parents[0].label)
 
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      Geolocation.requestAuthorization('always');
+    }
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocation({
+          latitude,
+          longitude,
+        });
+      },
+      (e) => {
+        console.log("error!!!!!!!!!!",e.code, e.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    )
+  }, []);
+  console.log("location", location)
+
+  
   return (
     <View style={styles.container}>
       
@@ -23,11 +49,20 @@ export default function Map() {
           provider={PROVIDER_GOOGLE}
           initialRegion={{
             latitude: 37.78825,
-            longitude: -122.4324,
+          longitude: -122.4324,
+            // latitude: location.latitude,
+            // longitude: location.longitude,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
+        >
+          <Marker
+          coordinate={{latitude: 37.78825, longitude: -122.4324}}
+          title="this is a marker"
+          description="this is a marker example"
         />
+        </MapView>
+        
       </View>
       <Text style={styles.title}>The last connection time</Text>
       <Text style={styles.date}>2022-10-11 18:32</Text>
