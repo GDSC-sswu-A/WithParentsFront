9a97c41eeddd319a1e6c4f0e4db9aa281899f596
Leeminhyeong <Leeminhyeong@MacBook-Pro.local>
2023-01-25 14:46:34 +0900

    #18 [feat] 지도 현재위치 적용하기

--- src/component/MapViewComponent.tsx
+++ src/component/MapViewComponent.tsx
@@ -1,8 +1,10 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
-import MapView, {PROVIDER_GOOGLE} from "react-native-maps"
+import MapView, {Marker, PROVIDER_GOOGLE} from "react-native-maps"
 
-export default function MapViewComponent() {
+export default function MapViewComponent(location : any, who: any) {
+  console.log(location, "Mapview Component")
+  console.log(location, "WHOOOOO")
     return (
         <>
           <View style={styles.container}>
@@ -10,12 +12,18 @@ export default function MapViewComponent() {
           style={styles.container}
           provider={PROVIDER_GOOGLE}
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: location.latitude,
+            longitude: location.longitude,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
-        />
+        >
+          <Marker
+            coordinate={{latitude: location.latitude, longitude: location.longitude}}
+            title={location.who.label}
+            description="recent location"
+          />
+        </MapView>
           </View>
         </>
       );
@@ -23,10 +31,6 @@ export default function MapViewComponent() {
 const styles = StyleSheet.create({
   container : {
    flex : 1,
-   backgroundColor : "white",
-   height : 200,
-   width :280,
-   padding : 100 
 },
   
 });
--- src/screens/Nav/Map.tsx
+++ src/screens/Nav/Map.tsx
@@ -1,21 +1,21 @@
-import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native'
+import { View, Text, StyleSheet, Label, TouchableOpacity, Platform } from 'react-native'
 import React, {useState, useEffect} from 'react'
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'
 import MapViewComponent from '../../component/MapViewComponent'
-import Geolocation from 'react-native-geolocation-service'
+import Geolocation, { PositionError } from 'react-native-geolocation-service'
 import DropDownPicker from 'react-native-dropdown-picker';
 
 interface ILocation {
   latitude : number;
   longitude : number;
 }
-export default function Map() {
-  const [location, setLocation] = useState<ILocation | undefined>(undefined);
+export default function Map(navigation) {
+  const [location, setLocation] = useState<ILocation | undefined>(37.78825,-122.4324);
   const [open, setOpen] = useState(false);
   const [parent, setParent] = useState(null);
   const [parents, setParents] = useState([
-      {label: '아부지', value: '1'},
-      {label: '어무니', value: '2'},
+      {label: '아부지', value: '0'},
+      {label: '어무니', value: '1'},
   ]);
 
   useEffect(() => {
@@ -29,41 +29,26 @@ export default function Map() {
           latitude,
           longitude,
         });
+        
       },
       (e) => {
         console.log("error!!!!!!!!!!",e.code, e.message);
       },
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
     )
+    
   }, []);
+  
   console.log("location", location)
-
   
   return (
     <View style={styles.container}>
       
       <Text style={styles.title}>Parent's location</Text>
       <View style={styles.map}>
-      <MapView
-          style={styles.container}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: 37.78825,
-          longitude: -122.4324,
-            // latitude: location.latitude,
-            // longitude: location.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          <Marker
-          coordinate={{latitude: 37.78825, longitude: -122.4324}}
-          title="this is a marker"
-          description="this is a marker example"
-        />
-        </MapView>
-        
-      </View>
+        <MapViewComponent location={location} who={parents[0]}/>
+    </View>
+      
       <Text style={styles.title}>The last connection time</Text>
       <Text style={styles.date}>2022-10-11 18:32</Text>
       <View>
@@ -99,7 +84,6 @@ const styles = StyleSheet.create({
   },
   map : {
     // flex : 1,
-    // margin : 10,
     height : 320,
     marginBottom : 40
   },
