4b91575b3524ff5f868293c63cd040038806c28d
정민 <90408458+jeonjmin@users.noreply.github.com>
2023-03-13 09:49:01 +0900

    #25 feat:테스트 알림 개발

--- App.tsx
+++ App.tsx
@@ -1,122 +1,134 @@
-import React from "react";
-import {StatusBar, Platform, Image} from 'react-native'
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { RecoilRoot, useRecoilState } from "recoil";
-import AddmedipageScreen from "./src/screens/Medicine/AddMediPage";
-import SettingScreen from "./src/screens/SettingScreen";
-import CreateFamilyScreen from "./src/screens/CreateFamilyScreen";
-import JoinFamilyScreen from "./src/screens/JoinFamilyScreen";
-import GlobalNav from "./src/screens/GlobalNav";
-import GalleryWriteScreen from "./src/screens/Gallery/GalleryWriteScreen"
-import WrittenGalleryScreen from "./src/screens/Gallery/WrittenGalleryScreen";
+import {StatusBar, Platform, Image} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {RecoilRoot, useRecoilState} from 'recoil';
+import AddmedipageScreen from './src/screens/Medicine/AddMediPage';
+import SettingScreen from './src/screens/SettingScreen';
+import CreateFamilyScreen from './src/screens/CreateFamilyScreen';
+import JoinFamilyScreen from './src/screens/JoinFamilyScreen';
+import GlobalNav from './src/screens/GlobalNav';
+import GalleryWriteScreen from './src/screens/Gallery/GalleryWriteScreen';
+import WrittenGalleryScreen from './src/screens/Gallery/WrittenGalleryScreen';
+
+import React, {useEffect} from 'react';
+
+import {
+  requestUserPermission,
+  NotificationListner,
+} from './src/utills/pushnotification_helper';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  
+  useEffect(() => {
+    requestUserPermission();
+    NotificationListner();
+  }, []);
   // if (Platform.OS === 'android') {
   //   StatusBar.setBackgroundColor('white');
   //   // StatusBar.setTranslucent(true);
   //   StatusBar.setBarStyle('dark-content');
   // }
 
+  //푸시알림(푸쉬 알림이 수신되었을 때 React Native 의 Alert api 를 사용해 알림을 띄우는 테스트 코드)
+  {
+    /*useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+}*/
+  }
+
   return (
     <RecoilRoot>
       {/* <StatusBar barStyle="dark-content" /> */}
-    <NavigationContainer>
-      <Stack.Navigator
-      screenOptions={{
-        
-      }}
-      initialRouteName="Nav">
-      <Stack.Screen
-      name="Nav"
-      component={GlobalNav}
-      options={{
-        headerShown : false
-      }}
-      />
-      <Stack.Screen
-      name="Setting"
-      component={SettingScreen}
-      options={({navigation})=>({
-        title : 'Setting',
-        headerTintColor: '#FFFFFF',
-        headerStyle : {
-          backgroundColor :'#AEC195',
-        },
-      })}
-      />
-     
-     <Stack.Screen
-      name="CreateFamily"
-      component={CreateFamilyScreen}
-      options={({navigation})=>({
-        title : 'Creating a New Family',
-        headerTintColor: '#FFFFFF',
-        headerStyle : {
-          backgroundColor :'#AEC195',
-        },
-      })}
-      />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{}} initialRouteName="Nav">
+          <Stack.Screen
+            name="Nav"
+            component={GlobalNav}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Setting"
+            component={SettingScreen}
+            options={({navigation}) => ({
+              title: 'Setting',
+              headerTintColor: '#FFFFFF',
+              headerStyle: {
+                backgroundColor: '#AEC195',
+              },
+            })}
+          />
 
-      <Stack.Screen
-      name="Addmedicine"
-      component={AddmedipageScreen}
-      options={({navigation})=>({
-        title : 'Add your medicine',
-        headerTintColor: '#FFFFFF',
-        headerStyle : {
-          backgroundColor :'#AEC195',
-        },
-      })}
-      />
+          <Stack.Screen
+            name="CreateFamily"
+            component={CreateFamilyScreen}
+            options={({navigation}) => ({
+              title: 'Creating a New Family',
+              headerTintColor: '#FFFFFF',
+              headerStyle: {
+                backgroundColor: '#AEC195',
+              },
+            })}
+          />
 
-      
-      <Stack.Screen
-      name="GalleryWrite"
-      component={GalleryWriteScreen}
-      options={({navigation})=>({
-        title : 'Write your memory',
-        headerTintColor: '#FFFFFF',
-        headerStyle : {
-          backgroundColor :'#AEC195',
-        },
-      })}
-      />
+          <Stack.Screen
+            name="Addmedicine"
+            component={AddmedipageScreen}
+            options={({navigation}) => ({
+              title: 'Add your medicine',
+              headerTintColor: '#FFFFFF',
+              headerStyle: {
+                backgroundColor: '#AEC195',
+              },
+            })}
+          />
 
-      <Stack.Screen
-      name="WrittenGalleryScreen"
-      component={WrittenGalleryScreen}
-      options={({navigation})=>({
-        title : 'Write your memory',
-        headerTintColor: '#FFFFFF',
-        headerStyle : {
-          backgroundColor :'#AEC195',
-        },
-      })}
-      />
+          <Stack.Screen
+            name="GalleryWrite"
+            component={GalleryWriteScreen}
+            options={({navigation}) => ({
+              title: 'Write your memory',
+              headerTintColor: '#FFFFFF',
+              headerStyle: {
+                backgroundColor: '#AEC195',
+              },
+            })}
+          />
 
-       <Stack.Screen
-      name="JoinFamily"
-      component={JoinFamilyScreen}
-      options={({navigation})=>({
-        title : 'Family registration',
-        headerTintColor: '#FFFFFF',
-        headerStyle : {
-          backgroundColor :'#AEC195',
-        },
+          <Stack.Screen
+            name="WrittenGalleryScreen"
+            component={WrittenGalleryScreen}
+            options={({navigation}) => ({
+              title: 'Write your memory',
+              headerTintColor: '#FFFFFF',
+              headerStyle: {
+                backgroundColor: '#AEC195',
+              },
+            })}
+          />
 
-      })}
-      />
-
-      </Stack.Navigator>
-   
-    </NavigationContainer>
-  </RecoilRoot>
-  )
+          <Stack.Screen
+            name="JoinFamily"
+            component={JoinFamilyScreen}
+            options={({navigation}) => ({
+              title: 'Family registration',
+              headerTintColor: '#FFFFFF',
+              headerStyle: {
+                backgroundColor: '#AEC195',
+              },
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RecoilRoot>
+  );
 }
-
 
 export default App;
---
+++ src/utills/pushnotification_helper.ts
@@ -0,0 +1,56 @@
+import messaging from '@react-native-firebase/messaging';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+    GetFCMToke();
+  }
+}
+
+async function GetFCMToke() {
+  let fcmtoken = await AsyncStorage.getItem('fcmtoken');
+  console.log(fcmtoken, 'old token');
+
+  if (!fcmtoken) {
+    try {
+      const fcmtoken = await messaging().getToken();
+      if (fcmtoken) {
+        console.log(fcmtoken, 'new token');
+        await AsyncStorage.setItem('fcmtoken', fcmtoken);
+      }
+    } catch (error) {
+      console.log(error, 'error in fcmtoken');
+    }
+  }
+}
+
+export const NotificationListner = () => {
+  messaging().onNotificationOpenedApp(remoteMessage => {
+    console.log(
+      'Notification caused app to open from background state:',
+      remoteMessage.notification,
+    );
+  });
+
+  messaging()
+    .getInitialNotification()
+    .then(remoteMessage => {
+      if (remoteMessage) {
+        console.log(
+          'Notification caused app to open from quit state:',
+          remoteMessage.notification,
+        );
+        setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+      }
+    });
+
+  messaging().onMessage(async remoteMessage => {
+    console.log('notification on state...', remoteMessage);
+  });
+};
